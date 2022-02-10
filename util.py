e9902dbc667e81aac71a4bf59ec622666e9a1db2
Carmen Wang <fw215@georgetown.edu>
2022-02-10 11:21:07 -0500

    in-class lab 1

--- util.py
+++ util.py
@@ -8,4 +8,11 @@ def parse_sts(data_file):
     texts = []
     labels = []
 
+    with open(data_file, "r") as df:
+        for line in df:
+            fields = line.strip().split("\t")
+            labels.append(float(fields[4])) # the score
+            t1 = fields[5].lower()
+            t2 = fields[6].lower()
+            texts.append((t1, t2))
     return texts, labels
